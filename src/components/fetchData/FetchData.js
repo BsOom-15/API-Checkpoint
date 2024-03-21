d166c247819a5fc859e54b6f293c57b002e753a5
BsOom-15 <ibtisamah15@gmail.com>
2024-03-21 04:09:37 +0200

    first commit

---
+++ src/components/fetchData/FetchData.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../fetchData/FetchData'
+
+const FetchData = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1 className='title'>User List</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FetchData;
